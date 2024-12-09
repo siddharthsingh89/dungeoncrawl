a8420adb3dd530fafaaedf09ea5e2837e2263795
Siddharth Singh <siddsingh@microsoft.com>
2024-12-09 18:12:39 +0530

    Added player details

--- src/map.rs
+++ src/map.rs
@@ -38,4 +38,20 @@ impl Map {
             }
         }
     }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_ids(point.x, point.y)] == TiltType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_ids(point.x, point.y))
+        }
+    }
 }
